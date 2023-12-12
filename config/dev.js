0e3b009db8b7526e42f922d055fd29bedead1cfb
omerHusni <omerhusni637@gmail.com>
2023-12-12 13:29:41 +0300

    Update environment configuration

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
- googleClientID: process.env.GOOGLE_CLIENT_ID,
- googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
- mongoURI: process.env.MONGO_DATABASE_URL,
- cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_DATABASE_URL,
+  cookieKey: process.env.COOKIE_KEY,
 };
