6a11736ff4527f9ed656483364980b96730b944e
omerHusni <omerhusni637@gmail.com>
2023-12-14 16:50:24 +0300

    Add header test and login oauth flow test

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,8 +14,16 @@ afterEach(async () => {
   await browser.close();
 });
 
-test('We can launch a browser', async () => {
+test('the header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('clicking login starting oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/account\.google\.com/);
 });
