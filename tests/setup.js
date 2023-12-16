bba72e9513bbc9c8e30b6f484cad407125c6da30
omerHusni <omerhusni637@gmail.com>
2023-12-16 23:28:50 +0300

    Add setup.js for testing environment

---
+++ tests/setup.js
@@ -0,0 +1,14 @@
+require('../models/User');
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
